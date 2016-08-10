1e16616637e32d254a2b045115f72609a5bbf2c4
Michał Gołębiowski <m.goleb@gmail.com>
2016-08-10 14:53:04 +0200

    Fix the build on Node.js 0.12
    ESLint 3.0 drops support for Node <4 so skip it there.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,16 @@ var semver = require('semver');
 module.exports = function (grunt) {
     require('time-grunt')(grunt);
 
+    var oldNode = /^v0\./.test(process.version);
+
+    // Support: Node.js <4
+    // Skip running tasks that dropped support for Node.js 0.10 & 0.12
+    // in those Node versions.
+    var runIfNewNode = function (task) {
+        return oldNode ? 'print_old_node_message:' + task : task;
+    };
+
+
     grunt.initConfig({
         eslint: {
             all: {
@@ -17,8 +27,18 @@ module.exports = function (grunt) {
         },
     });
 
-    // Load all grunt tasks matching the `grunt-*` pattern.
-    require('load-grunt-tasks')(grunt);
+    // Load grunt tasks from NPM packages
+    // Support: Node.js <4
+    // Don't load the eslint task in old Node.js, it won't parse.
+    require('load-grunt-tasks')(grunt, {
+        pattern: oldNode ? ['grunt-*', '!grunt-eslint'] : ['grunt-*'],
+    });
+
+    // Supports: Node.js <4
+    grunt.registerTask('print_old_node_message', function () {
+        var task = [].slice.call(arguments).join(':');
+        grunt.log.writeln('Old Node.js detected, running the task "' + task + '" skipped...');
+    });
 
     grunt.registerTask('asserts', function () {
         try {
@@ -48,7 +68,7 @@ module.exports = function (grunt) {
     });
 
     grunt.registerTask('lint', [
-        'eslint',
+        runIfNewNode('eslint'),
     ]);
 
     grunt.registerTask('test', [
